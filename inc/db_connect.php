9940d411fdd03bbd492b155e9b7c803bbb7333fd
akram mahmoud <akrammahmoud.web@gmail.com>
2022-06-04 18:03:01 +0200

    db connection

--- inc/db_connect.php
+++ inc/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_hostname = "localhosts";
-$mysql_user = "roots";
-$mysql_password = "";
-$mysql_database = "palmis";
+$mysql_hostname = $_server['hostname'];
+$mysql_user = $_server['RDS_USERNAME'];
+$mysql_password = $_server['RDS_PASSWORD'];
+$mysql_database = $_server['RDS_DB_NAME'];
 
 
 $db_connect = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or ("Could not connect database");
