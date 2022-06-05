406dac6d1b24ccde2fb87c0d60ebd10d44453aa3
akram mahmoud <akrammahmoud.web@gmail.com>
2022-06-05 10:57:56 +0200

    t

--- employee.php
+++ employee.php
@@ -4,5 +4,4 @@ session_start();
 if (!isset($_SESSION['lang'])) {
     $_SESSION['lang'] = 'en';
 }
-echo $_SESSION['lang'];
-require($_SERVER['DOCUMENT_ROOT'] . "lang/{$_SESSION['lang']}/employee.php");
+require("lang/{$_SESSION['lang']}/employee.php");
