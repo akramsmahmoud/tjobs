30e4878f9371537dce5d5511c64ee0e62b6603c6
akram mahmoud <akrammahmoud.web@gmail.com>
2022-06-05 10:52:18 +0200

    require

--- employee.php
+++ employee.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
-echo $_SERVER['PHP_SELF'];
-echo $_SERVER['RDS_HOSTNAME'];
 if (!isset($_SESSION['lang'])) {
     $_SESSION['lang'] = 'en';
 }
-
-include("lang/{$_SESSION['lang']}/employee.php");
+echo $_SESSION['lang'];
+require($_SERVER['DOCUMENT_ROOT'] . "lang/{$_SESSION['lang']}/employee.php");
