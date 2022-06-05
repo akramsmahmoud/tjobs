bc2eafb2ed9287319ae5d8a629f1cb5c5817edd2
akram mahmoud <akrammahmoud.web@gmail.com>
2022-06-05 15:54:24 +0200

    s

--- js/script.js
+++ js/script.js
@@ -121,17 +121,18 @@ $(function () {
 /* ==============================================
   	  Nice Scroll 
   	=============================================== */
-
-$("html").niceScroll({
-  scrollspeed: 100,
-  mousescrollstep: 38,
-  cursorwidth: 5,
-  cursorborder: 0,
-  cursorcolor: "#333",
-  autohidemode: true,
-  zindex: 999999999,
-  horizrailenabled: false,
-  cursorborderradius: 0,
+$(document).ready(function () {
+  $("html").niceScroll({
+    scrollspeed: 100,
+    mousescrollstep: 38,
+    cursorwidth: 5,
+    cursorborder: 0,
+    cursorcolor: "#333",
+    autohidemode: true,
+    zindex: 999999999,
+    horizrailenabled: false,
+    cursorborderradius: 0,
+  });
 });
 
 /* ==============================================
