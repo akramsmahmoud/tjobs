3b78300a78bcd24ff4bba40b6714cac87817a5d9
akram mahmoud <akrammahmoud.web@gmail.com>
2022-06-05 10:45:29 +0200

    debug

--- employee.php
+++ employee.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
-
+echo $_SERVER['PHP_SELF'];
+echo $_SERVER['RDS_HOSTNAME'];
 if (!isset($_SESSION['lang'])) {
     $_SESSION['lang'] = 'en';
 }
 
-//include("lang/{$_SESSION['lang']}/employee.php");
-
-
-echo $_SESSION['lang'];
+include("lang/{$_SESSION['lang']}/employee.php");
