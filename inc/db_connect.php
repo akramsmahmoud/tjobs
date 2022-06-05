52eee71f2c9c15ed5bd0effd4caf35399b9cb2ea
akram mahmoud <akrammahmoud.web@gmail.com>
2022-06-05 10:26:43 +0200

    error log

--- inc/db_connect.php
+++ inc/db_connect.php
@@ -1,12 +1,22 @@
 <?php
-$mysql_hostname = $_SERVER['RDS_HOSTNAME'];
-$mysql_user = $_SERVER['RDS_USERNAME'];
-$mysql_password = $_SERVER['RDS_PASSWORD'];
-$mysql_database = $_SERVER['RDS_DB_NAME'];
-$mysql_port = $_SERVER['RDS_PORT'];
-
-
-$db_connect = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database, $mysql_port);
-if (!$db_connect) {
-    throw new Error("Could not connect database");
+if (isset($_SERVER['RDS_HOSTNAME'])) {
+    $mysql_hostname = $_SERVER['RDS_HOSTNAME'];
+    $mysql_user = $_SERVER['RDS_USERNAME'];
+    $mysql_password = $_SERVER['RDS_PASSWORD'];
+    $mysql_database = $_SERVER['RDS_DB_NAME'];
+    $mysql_port = $_SERVER['RDS_PORT'];
+} else {
+    $mysql_hostname = 'localhost';
+    $mysql_user = 'root';
+    $mysql_password = '';
+    $mysql_database = 'palmi';
+    $mysql_port = null;
+}
+try {
+    $db_connect = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database, $mysql_port);
+    if (!$db_connect) {
+        throw new Error("Could not connect database");
+    }
+} catch (Exception $e) {
+    echo 'and the error is: ',  $e->getMessage(), "\n";
 }
