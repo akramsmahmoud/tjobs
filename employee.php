40c8f320aed96ea174932caad4c02fd31afeecd1
akram mahmoud <akrammahmoud.web@gmail.com>
2022-06-12 13:48:19 +0200

    go

--- employee.php
+++ employee.php
@@ -1,7 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-session_start();
-if (!isset($_SESSION['lang'])) {
-    $_SESSION['lang'] = 'en';
+$lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : 'en';
+if (isset($_GET['lang'])) {
+    $get_lang = $_GET['lang'];
+    $whitelist_langs = ['en', 'ar', 'it', 'gr'];
+
+    $lang = in_array($get_lang, $whitelist_langs) ? $get_lang : 'en';
 }
+$_SESSION['lang'] =  $lang;
 require("lang/{$_SESSION['lang']}/employee.php");
--- joboffers.php
+++ joboffers.php
@@ -1,3 +1,10 @@
 <?php
-if (!isset($_SESSION['lang'])) $_SESSION['lang'] = 'en';
+$lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : 'en';
+if (isset($_GET['lang'])) {
+    $get_lang = $_GET['lang'];
+    $whitelist_langs = ['en', 'ar', 'it', 'gr'];
+
+    $lang = in_array($get_lang, $whitelist_langs) ? $get_lang : 'en';
+}
+$_SESSION['lang'] =  $lang;
 include("lang/{$_SESSION['lang']}/joboffers.php");
