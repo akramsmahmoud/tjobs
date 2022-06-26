f1949564d880054a41826e7009be539dac8f5d1f
akramsmahmoud <akrammahmoud.web@gmail.com>
2022-06-26 15:17:07 +0200

    dockerize the project

--- web/public/inc/db_connect.php
+++ web/public/inc/db_connect.php
@@ -6,19 +6,18 @@ if (isset($_SERVER['RDS_HOSTNAME'])) {
     $mysql_database = $_SERVER['RDS_DB_NAME'];
     $mysql_port = $_SERVER['RDS_PORT'];
 } else {
-    $mysql_hostname = 'localhost';
-    $mysql_user = 'root';
-    $mysql_password = '';
-    $mysql_database = 'palmi';
+    $mysql_hostname = 'mysql';
+    $mysql_user = 'dev';
+    $mysql_password = 'dev';
+    $mysql_database = 'tjobs';
     $mysql_port = null;
 }
 try {
-    if ($mysql_password !== 'akrampassword') {
-        $mysql_password = 'akrampassword';
-    }
     $db_connect = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database, $mysql_port);
     if (!$db_connect) {
-        throw new Error("Could not connect database");
+        echo mysqli_connect_errno() . ":" . mysqli_connect_error();
+
+        throw new Error("Could not connect databases");
     }
 } catch (Exception $e) {
     echo 'and the error is: ',  $e->getMessage(), "\n";
